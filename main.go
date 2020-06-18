29e9eda18637299dd38aac947d5bd82f0e19ec67
Vladislav Ponomarev <farazeus@gmail.com>
2020-06-18 22:15:54 +0300

    Make Telegram Bot working

--- main.go
+++ main.go
@@ -1,19 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
 	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
 )
 
-func whatthedrink(num int) string {
-	if num%2 == 0 {
-		return "чй"
-	} else {
-		return "кф"
-	}
+func init() {
+    if err := godotenv.Load(); err != nil {
+        log.Print("No .env file found")
+    }
 }
 
+func whatthedrink() string {
+	var opts  = strings.Split("чй,кф", ",")
+	rand.Seed(time.Now().UnixNano())
+	return opts[rand.Intn(len(opts))]
+}
+
+var numericKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("Хочу попить!"),
+	),
+)
+
 func main() {
-	hours, minutes, seconds := time.Now().Clock()
-	fmt.Println(whatthedrink(hours + minutes + seconds - 5))
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, whatthedrink())
+		msg.ReplyMarkup = numericKeyboard
+		bot.Send(msg)
+	}
 }
